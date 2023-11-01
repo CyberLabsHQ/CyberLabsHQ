08cfe46587392bd175dbf3a590816f5783344b52
ElormCodes1 <107018207+ElormCodes1@users.noreply.github.com>
2023-11-01 18:46:05 +0000

    Initial commit

---
+++ src/app/(portfolio)/me/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+      <section
+        id="home"
+        className="relative z-10 overflow-hidden pt-[120px] pb-16 md:pt-[150px] md:pb-[120px] xl:pt-[180px] xl:pb-[160px] 2xl:pt-[210px] 2xl:pb-[200px]"
+      >
+        <h1>Home</h1>
+      </section>
+    </>
+  );
+}
