0ecc486a462bd8ed833d9463ceb549298a5b8751
ElormCodes1 <marriondokosi@gmail.com>
2023-11-06 00:14:06 +0000

    portfolio work

--- src/app/(portfolio)/me/page.tsx
+++ src/app/(portfolio)/me/page.tsx
@@ -1,12 +1,17 @@
+import React from "react";
+import Pabout from "@/components/Pabout";
+import Phero from "@/components/Phero";
+import Skills from "@/components/Skills";
+
 export default function Home() {
   return (
-    <>
-      <section
-        id="home"
-        className="relative z-10 overflow-hidden pt-[120px] pb-16 md:pt-[150px] md:pb-[120px] xl:pt-[180px] xl:pb-[160px] 2xl:pt-[210px] 2xl:pb-[200px]"
-      >
-        <h1>Home</h1>
-      </section>
-    </>
+    <section
+      id="home"
+      className="relative z-10 overflow-hidden pt-[120px] pb-16 md:pt-[150px] md:pb-[120px] xl:pt-[180px] xl:pb-[160px] 2xl:pt-[210px] 2xl:pb-[200px]"
+    >
+      <Phero />
+      <Pabout />
+      <Skills />
+    </section>
   );
 }
