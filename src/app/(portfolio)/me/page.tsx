d136bd087339d407a1281cbd59f881832f40e0f3
ElormCodes1 <marriondokosi@gmail.com>
2023-11-06 11:32:16 +0000

    sdf

--- src/app/(portfolio)/me/page.tsx
+++ src/app/(portfolio)/me/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
       className="relative z-10 overflow-hidden pt-[120px] pb-16 md:pt-[150px] md:pb-[120px] xl:pt-[180px] xl:pb-[160px] 2xl:pt-[210px] 2xl:pb-[200px]"
     >
       <Phero />
+      hello
       <Pabout />
       <Skills />
     </section>
